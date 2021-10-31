2a9f1e2867657efbdcab65c629dbc804008a23d2
drethenrain <drethenrain@hotmail.com>
2021-10-31 17:25:44 -0300

    :sparkles: Auth

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,38 @@
+import { prisma } from '../prisma/client';
+import { sign, verify as JWTVerify } from 'jsonwebtoken';
+import type { Request, Response, NextFunction } from 'express';
+
+class AuthController {
+  async login(req: Request, res: Response) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!user) return res.json({ error: 'user not found' });
+
+    const token = sign({ id: user.id }, process.env.SECRET as string, {
+      expiresIn: 86400,
+    });
+
+    return res.json({ user, token });
+  }
+
+  async verify(req: Request, res: Response, next: NextFunction) {
+    const token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({ error: 'No token provided' });
+
+    return JWTVerify(
+      token as string,
+      process.env.SECRET as string,
+      async (err) => {
+        if (err)
+          return res.status(401).send({ error: 'Token provided is invalid' });
+        return next();
+      }
+    );
+  }
+}
+
+export default new AuthController();
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import AuthController from './controllers/AuthController';
 import UserController from './controllers/UserController';
 
 export const route = Router();
@@ -8,4 +9,7 @@ route
   .get('/users/:id', UserController.findById)
   .post('/users', UserController.create)
   .put('/users/:id', UserController.update)
-  .delete('/users/:id', UserController.delete);
+  .delete('/users/:id', UserController.delete)
+
+  // Auth
+  .post('/login', AuthController.login);
